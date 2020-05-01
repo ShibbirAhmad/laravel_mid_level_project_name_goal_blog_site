f5c73983cc11f169b3e368212322f0565bc0aa2b
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-05-01 09:12:30 +0600

    userDashboard developed and linked with every post  project completed

--- resources/views/site/userDashboard.blade.php
+++ resources/views/site/userDashboard.blade.php
@@ -3,26 +3,35 @@
 
 @section('content')
 <div class="container  bg-light ">
-    <div class="row">
-         <div class="col-md-10">
-            <div class="panel mt-5 panel-default">
-                <div class="panle-heading">
-                    <div class="panle-title"><h4>your latest comment</h4></div>
-                </div>
-          
-                <div class="panel-body">
-                     <table class="table bg-success table-bordered">
-                               @foreach ($comments as $comment)
-                                   <tr>
-                                       <td>{{$comment->comment}}</td>
-                                       <td>{{$comment->status}}</td>
-                                   </tr>
-                               @endforeach
-                     </table>
-                </div>
-            </div>
-          
-         </div>
+<div class="row">
+ <div class="col-md-10">
+    <div class="panel mt-5 panel-default">
+        <div class="panle-heading">
+            <div class="panle-title"><h4>your latest comment</h4></div>
+        </div>
+    
+        <div class="panel-body">
+                <table class="table bg-secondary table-bordered">
+                <thead>
+                        <th>Date</th>
+                        <th>post title</th>
+                        <th>comment</th>
+                        <th>status</th>
+                
+                </thead>      
+    @foreach ($comments as $comment)
+                <tr>
+                    <td>{{date('d-m-Y',strtotime($comment->created_at)) }}</td>
+                    <td><a style="text-decoration:none; color:#fff;" href="{{route('singleRoute',$comment->post->id)}}" target="_blank" > {{$comment->post->postTitle}}</a></td>
+                    <td>{{$comment->comment}}</td>
+                    <td>{{$comment->status}}</td>
+                </tr>
+            @endforeach
+                </table>
+        </div>
+    </div>
+    
+    </div>
 
 
          {{-- //this for user profile info --}}
